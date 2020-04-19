f498d5c3d674b69af860ce32dbec93953aee1e79
Anika <apatelcse@gmail.com>
2020-04-19 11:32:43 -0700

    Added boilerplate elements + Babel.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-let canvas = document.getElementById("gameScreen");
-
-let ctx = canvas.getContext('2d');
-
-ctx.clearRect(0, 0, 800, 600);
-
+;(function () {
+    console.log('IIFE JS Loaded!')
+  })()
